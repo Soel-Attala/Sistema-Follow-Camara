fd0074c3c60cfed1205c998ceacd71d153f62c51
Soel Antonio Attala <95297206+Soel-Attala@users.noreply.github.com>
2022-07-05 19:26:31 -0300

    Agregamos movimiento al jugador y sistema de camaras

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float hor;
+    public float ver;
+    public float speed;
+    public GameObject camUno;
+    public GameObject camDos;
+    public float rotateSpeed;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MovimientoJugador();
+        PlayerMoveMentRE();             
+    }
+
+    void MovimientoJugador()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hor, 0, ver) * speed * Time.deltaTime);
+    }
+
+    void PlayerMoveMentRE()
+    {
+        float ver = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, ver) * speed * Time.deltaTime);
+
+        float hor = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, hor, 0) * rotateSpeed * Time.deltaTime);
+     }
+}
